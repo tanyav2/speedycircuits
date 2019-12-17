d0630376368319b1af9f398559531622af8af180
stratosphericvibes <tanyav2@illinois.edu>
2019-12-16 18:18:28 -0600

    can parse inputs, basic ckt done

--- codegen.py
+++ codegen.py
@@ -1,4 +1,20 @@
-cons = dict(ADD=[(66, 0)], SORT=[(641, 1)], MILL=[(65, 1), (98, 1)],
+import argparse
+
+cons = dict(ADD=[(66, 0)],
+            SORT=[(641, 1), (678, 1), (1069, 1), (1458, 1), (1847, 1), (2236, 1), (2625, 1), (3014, 1), (3403, 1),
+                  (3792, 1), (4183, 1), (4574, 1), (4963, 1), (5352, 1), (5741, 1), (6130, 1), (6519, 1), (6908, 1),
+                  (7297, 1), (7686, 1), (8075, 1), (8464, 1), (8853, 1), (9242, 1), (9631, 1), (10020, 1), (10409, 1),
+                  (10798, 1), (11187, 1), (11576, 1), (11965, 1), (12354, 1), (12743, 1), (13132, 1), (13524, 1),
+                  (13915, 1), (14304, 1), (14693, 1), (15082, 1), (15471, 1), (15860, 1), (16249, 1), (16638, 1),
+                  (17029, 1), (17420, 1), (17809, 1), (18198, 1), (18587, 1), (18976, 1), (19365, 1), (19754, 1),
+                  (20143, 1), (20532, 1), (20921, 1), (21310, 1), (21699, 1), (22088, 1), (22477, 1), (22866, 1),
+                  (23255, 1), (23644, 1), (24033, 1), (24422, 1), (24811, 1), (25200, 1), (25589, 1), (25978, 1),
+                  (26367, 1), (26756, 1), (27145, 1), (27534, 1), (27923, 1), (28312, 1), (28701, 1), (29090, 1),
+                  (29479, 1), (29868, 1), (30257, 1), (30646, 1), (31035, 1), (31424, 1), (31813, 1), (32202, 1),
+                  (32591, 1), (32980, 1), (33369, 1), (33758, 1), (34147, 1), (34536, 1), (34925, 1), (35314, 1),
+                  (35703, 1), (36092, 1), (36481, 1), (36870, 1), (37259, 1), (37648, 1), (38037, 1), (38426, 1),
+                  (38815, 1), (39204, 1), (39593, 1), (39982, 1), (40371, 1), (40760, 1), (41149, 1), (41538, 1)]
+            , MILL=[(65, 1), (98, 1)],
             MULT=[(64, 0), (258, 0), (515, 0), (772, 0), (1029, 0), (1286, 0), (1543, 0), (1800, 0), (2057, 0),
                   (2314, 0), (2571, 0), (2828, 0), (3085, 0), (3342, 0), (3599, 0), (3856, 0), (4113, 0), (4370, 0),
                   (4627, 0), (4884, 0), (5141, 0), (5398, 0), (5655, 0), (5912, 0), (6169, 0), (6426, 0), (6683, 0),
@@ -8,13 +24,40 @@ cons = dict(ADD=[(66, 0)], SORT=[(641, 1)], MILL=[(65, 1), (98, 1)],
                  (272, 0), (278, 0), (289, 0), (305, 0), (311, 0), (317, 0), (328, 0), (334, 0), (340, 0), (351, 0),
                  (367, 0), (388, 0), (414, 0)])
 
-# for add, mult, mill
-x = 2
-y = 5
-op = "MULT"
 
-alice_in = (f'{x:032b}')[::-1]
-bob_in = (f'{y:032b}')[::-1]
+parser = argparse.ArgumentParser(description='Generate optimized LLVM IR to evaluate boolean circuits.')
+parser.add_argument('filename', type=str, help='File containing netlist to parse')
+parser.add_argument('-alice_in', type=int, help='Alice\'s input')
+parser.add_argument('-bob_in', type=int, help='Bob\'s input')
+args = parser.parse_args()
+
+# get opcode
+with open(args.filename) as fp:
+    first_line = fp.readline()
+    second_line = fp.readline()
+    third_line = fp.readline()
+
+opcodes = {"Add": "ADD", "Sort": "SORT", "Millionaire": "MILL", "Mult": "MULT", "HammingDistance": "HAM"}
+op = None
+for opcode in opcodes:
+    if first_line.find(opcode) >= 0:
+        op = opcodes[opcode]
+
+# get inputs
+alice_in = None
+bob_in = None
+file_idx = 0
+if op == "SORT" or op == "HAM":
+    alice_in = ''.join(['1' if x == "true" else '0' for x in second_line.split(":")[1].replace("[", "").replace("]", "").replace(" ", "").split(",")])
+    bob_in = ''.join(['1' if x == "true" else '0' for x in third_line.split(":")[1].replace("[", "").replace("]", "").replace(" ", "").split(",")])
+    file_idx = 3
+else:
+    alice_in = f'{args.alice_in:032b}'[::-1]
+    bob_in = f'{args.bob_in:032b}'[::-1]
+    file_idx = 1
+
+
+# print globals
 alice_in_str = "@alice_in = global [32 x i32] ["
 bob_in_str = "@bob_in = global [32 x i32] ["
 for i, (a_bit, b_bit) in enumerate(zip(alice_in, bob_in)):
@@ -63,8 +106,9 @@ for i, k in enumerate(constants):
 print("\n")
 
 # all the registers
-filename = "mult.txt"  # parse this from file
-with open("mult.txt") as fp:
+with open(args.filename) as fp:
+    for i in range(file_idx):
+        next(fp)
     for line in fp:
         line = line.rstrip()
         words = line.split(" ")
@@ -73,3 +117,4 @@ with open("mult.txt") as fp:
 
 print("\n\t%result = zext i32 %w69 to i64")
 print("\tret i64 %result\n}")
+
